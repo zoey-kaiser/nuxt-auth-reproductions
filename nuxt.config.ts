16613d302a324e16cabf14a6975756380d9ddaf6
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-07-16 18:24:15 +0200

    Added basic setup for refresh provider

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,40 @@ export default defineNuxtConfig({
   modules: [
     '@sidebase/nuxt-auth'
   ],
+
+  auth: {
+    provider: {
+      type: 'refresh',
+      // refreshOnlyToken: true,
+      endpoints: {
+        getSession: { path: '/session', method: 'get' },
+        refresh: { path: '/refresh', method: 'post' }
+      },
+      pages: {
+        login: '/login'
+      },
+      token: {
+        signInResponseTokenPointer: '/token/accessToken',
+        maxAgeInSeconds: 60 * 5, // 5 min
+        sameSiteAttribute: 'lax'
+      },
+      refreshToken: {
+        signInResponseRefreshTokenPointer: '/token/refreshToken',
+        refreshRequestTokenPointer: '/refreshToken'
+      }
+    },
+    globalAppMiddleware: {
+      isEnabled: true
+    }
+  },
+
+  build: {
+    transpile: ['jsonwebtoken']
+  },
+
   typescript: {
     shim: false
-  }
+  },
+
+  compatibilityDate: '2024-07-16'
 })
---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+definePageMeta({
+  auth: false
+})
+
+const demoCredentials = {
+  username: 'user',
+  password: 'hunter2',
+}
+
+const { signIn, data, signOut, status } = useAuth()
+</script>
+
+<template>
+  <h2>Status: {{ status }}</h2>
+
+  <button @click="signIn(demoCredentials)">
+    Sign In
+  </button>
+  <button @click="signOut()">
+    Sign Out
+  </button>
+
+  <pre>
+    {{ data }}
+  </pre>
+</template>
